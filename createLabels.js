dfd331556eb4a558a63fadf8e4f778cd62c034f5
FTC_CodeABot <codeabot.ftc@gmail.com>
2024-09-30 16:47:51 +0200

    update lol

--- createLabels.js
+++ createLabels.js
@@ -1,10 +1,11 @@
 import * as fs from "fs/promises";
 import * as ejs from "ejs";
 
+const args = process.argv.slice(2);
 const contentsList = JSON.parse(
-	await fs.readFile("contentsList.json", "utf-8")
+	await fs.readFile(args[0] || "contentsList.json", "utf-8")
 );
 
-const labels = await ejs.renderFile("label.ejs", { contentsList });
+const labels = await ejs.renderFile("label.ejs", { contentsList: contentsList.list });
 
 await fs.writeFile("labels.html", labels);
